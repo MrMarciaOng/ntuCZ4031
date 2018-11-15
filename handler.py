471dceb69838e2103836aa0f275c3cb5c34f983d
nkarinka001 <nkarinka9@gmail.com>
2018-11-15 11:59:35 +0800

    connection established

--- handler.py
+++ handler.py
@@ -9,18 +9,36 @@ from query_handler import connect, query_handler
     2: Input: Plan
 """
 
+defaultDBData = {
+    "db_name" : "CZ4031",
+    "user" : "postgres",
+    "password" : "postgres",
+    "host" : "localhost",
+    "port" : "5432"
+}
+
 def handler(payload):
     action_type = payload["type"]
     data = payload["data"]
 
-    if action_type == 0:
+    if action_type == 0: 
         result = connect(data)
         if result:
+            defaultDBData["db_name"] = data["db_name"]
+            defaultDBData["user"] = data["user"]
+            defaultDBData["password"] = data["password"]
+            defaultDBData["host"] = data["host"]
+            defaultDBData["port"] = data["port"]
             return "Connection Successful!"
         else:
             return "Unable to establish connection to the database, Please check your credentials!"
 
     elif action_type == 1:
+        data["db_name"] = defaultDBData["db_name"]
+        data["user"] = defaultDBData["user"]
+        data["password"] = defaultDBData["password"]
+        data["host"] = defaultDBData["host"]
+        data["port"] = defaultDBData["port"]
         result = query_handler(data)
         return result 
     
